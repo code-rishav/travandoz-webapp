001ba1c4c443e641982bab95b855daa7fe5ba5a0
premdeshmukh18 <premdeshmukh1818@ggmail.com>
2023-09-02 01:07:54 +0530

    Navbar is added
    Navbar is added.
    Basic navbar is created. please review it once and help me to adjust the colors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 
 import ContactUs from './components/ContactUs'
 import Footer from './components/Footer'
-
+import Navbar from './components/Navbar'
 
 function App() {
 
 
   return (
     <>
+      <Navbar/>
       <ContactUs />
       <Footer />
+     
     </>
 
   )
